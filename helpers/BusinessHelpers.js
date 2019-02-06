2ceb36e6f2a84b5c33361ac8f2174837bd9023ae
Carlo Clamucha <carlo.clamucha@gmail.com>
2019-02-06 11:26:52 -0600

    added crud operations on the business

--- helpers/BusinessHelpers.js
+++ helpers/BusinessHelpers.js
@@ -6,3 +6,35 @@ exports.addBusiness = async (req, res) => {
   });
   res.json({ message: 'Successfully Created a New Store' });
 };
+
+exports.getBusiness = async (req, res) => {
+  const allBusinesses = await Business.find();
+  try {
+    res.json(allBusinesses);
+  } catch (error) {
+    res.json({ message: 'internal service error' });
+  }
+};
+
+exports.updateBusiness = async (req, res) => {
+  const allBusinesses = await Business.findOneAndUpdate(
+    { id: req.params.id },
+    req.body,
+    { new: true, runValidators: true }
+  ).exec();
+
+  try {
+    res.json({ message: `Business with id of ${id}` });
+  } catch (error) {
+    res.json({ message: 'could not update the business' });
+  }
+};
+
+exports.deleteBusiness = async (req, res) => {
+  await Business.findByIdAndDelete(req.params.id);
+  try {
+    res.json(allBusinesses);
+  } catch (error) {
+    res.json({ message: 'internal service error' });
+  }
+};
